43693f7ec7a1ea3524c42ed4c263a563523f6ef4
CharlesGarrocho <charles.garrocho@gmail.com>
2013-07-11 20:04:09 -0300

    chenged: name module handler to manager in views.

--- image_manager/views.py
+++ image_manager/views.py
@@ -2,7 +2,7 @@
 from django.http import HttpResponse
 from django.utils import simplejson
 from django.conf import settings
-import handler
+import manager
 import mimetypes
 
 
@@ -10,9 +10,9 @@ def add(request):
     response, parameters = validate_request(request)
 
     if response.status_code == 204:
-        if handler.delete(parameters['address'], parameters['name']):
+        if manager.delete(parameters['address'], parameters['name']):
             if 'file' in request.FILES:
-                if handler.add(image, address, name):
+                if manager.add(image, address, name):
                     response.status_code = 200
     return response
 
@@ -21,7 +21,7 @@ def load(request):
     response, parameters = validate_request(request)
 
     if response.status_code == 204:
-        image = handler.load(parameters['address'], parameters['name'])
+        image = manager.load(parameters['address'], parameters['name'])
         if image is not None:
             mimetype, encoding = mimetypes.guess_type(image)
             response = HttpResponse(mimetype=mimetype)
@@ -35,7 +35,7 @@ def list(request):
     response, parameters = validate_request(request)
 
     if response.status_code == 204:
-        lista_img = handler.list(parameters['address'], parameters['name'])
+        lista_img = manager.list(parameters['address'], parameters['name'])
         if len(lista_img) > 0:
             response = HttpResponse(simplejson.dumps({'images': lista_img}), mimetype='application/json')
             response.encoding = 'utf-8'
@@ -47,7 +47,7 @@ def delete(request):
     response, parameters = validate_request(request)
 
     if response.status_code == 204:
-        if handler.delete(parameters['address'], parameters['name']):
+        if manager.delete(parameters['address'], parameters['name']):
             response.status_code = 200
     return response
 
@@ -56,7 +56,7 @@ def validate_request(request):
     response = HttpResponse()
     response.status_code = 204
     parameters = None
-    if request.method == settings.HANDLER_IMAGES_METHOD:
+    if request.method == settings.MANAGER_IMAGES_METHOD:
         if request.method == 'POST':
             if 'address' in request.POST and 'name' in request.POST:
                 parameters = {'address':request.POST['address'], 'name':request.POST['name']}

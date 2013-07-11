010ec8cda85f86b6ab0d815b838bc40cc272fe18
CharlesGarrocho <charles.garrocho@gmail.com>
2013-07-11 20:02:10 -0300

    fixed: name directory app in url.

--- image_manager/urls.py
+++ image_manager/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('handler_images',
+urlpatterns = patterns('image_manager',
     url(r'^add$', 'views.add', name='add'),
     url(r'^load$', 'views.load', name='load'),
     url(r'^list$', 'views.list', name='list'),

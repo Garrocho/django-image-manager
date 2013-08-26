7d498fab7c85bb98c83e7a9ca664a740eeddaa0b
CharlesGarrocho <charles.garrocho@gmail.com>
2013-08-26 08:53:38 -0300

    added: documentation module init from package.

---
+++ image_manager/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @author: Charles Tim Batista Garrocho
+# @contact: charles.garrocho@gmail.com
+# @copyright: (C) 2013 Python Software Open Source
+
+"""
+This is the module init package design.
+"""
+
+
+__author__  = 'Charles Tim Batista Garrocho'
+
+__version__ = '0.0.5'
+
+__all__ = [
+    'manager.py',
+    'urls.py',
+    'views.py',
+]
